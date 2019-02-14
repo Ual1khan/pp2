6599c647b317c995b39c5939d21d76183cb4b16b
Ual1khan <ual1han999@gmail.com>
2019-02-14 16:49:16 +0600

    Week2 Task1

---
+++ Week2/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public static bool pl(string s)
+        {
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                    return false;
+                    }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            string s = System.IO.File.ReadAllText("/Users/ualihan/Desktop/pp2/Week2/Task1/test.txt");
+            if (pl(s))
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+        }
+    }
+}
